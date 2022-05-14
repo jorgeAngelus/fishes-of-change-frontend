4645fc9d196a105a605831c25927d2a19e0d3df1
Beto-cpu <beto.munguia.lara@gmail.com>
2022-05-14 12:43:02 -0700

    Responsive header

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className="w-full">
             <div className="w-full bg-gray-800 flex justify-center">
                 <div className="max-w-7xl w-full mx-2">
-                    <div className="flex flex-row justify-start items-center py-2">
+                    <div className="flex flex-row justify-center sm:justify-start items-center py-2">
                         <Link href="/">
                             <a>
                                 <div className="w-24 transform hover:scale-105">
@@ -16,7 +16,7 @@ export default function Header() {
                             </a>
                         </Link>
                         <Link href="/">
-                            <a className="ml-3 text-white text-2xl font-bold hover:text-yellow-300 hover:scale-105">
+                            <a className="ml-3 text-white text-2xl font-bold hover:text-yellow-300 hover:scale-105 hidden sm:block">
                                 Fishes Of Change
                             </a>
                         </Link>
@@ -24,8 +24,8 @@ export default function Header() {
                 </div>
             </div>
             <div className="w-full bg-blue-400 flex justify-center">
-                <div className="flex justify-end max-w-7xl w-full mx-2 py-2 ">
-                    <ul className="flex flex-row space-x-5 text-white font-bold">
+                <div className="flex justify-center sm:justify-end max-w-7xl w-full mx-2 py-2">
+                    <ul className="flex flex-col sm:flex-row items-center space-x-5 text-white font-bold">
                         <li>
                             <Link href="/">
                                 <a className="hover:text-yellow-300 hover:underline">Click The Oil</a>
